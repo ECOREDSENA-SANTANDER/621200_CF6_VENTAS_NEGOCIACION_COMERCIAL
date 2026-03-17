089964bec3214a3e2166669397021e165e37829e
lizmanchego <karinamanchego0416@gmail.com>
2026-03-17 09:48:42 -0500

    [ADD] Tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -18,7 +18,7 @@
         .row.bgavatarazulclaro(data-aos="fade-left")
           p.mt-5.mb-5.ps-5 #[b Por parte de la empresa], para construir relaciones con los clientes se debe montar una estrategia conducente a que los proveedores, los clientes, distribuidores, comerciantes y públicos interesados mantengan intercambios de interés, precios y de calidad a lo largo del tiempo en la cadena de valor. 
         .row.bgblanco.mt-3
-        .row.bgrosado(data-aos="fade-left")
+        .row.bg-color-10(data-aos="fade-left")
           p.mt-5.mb-5.ps-5 #[b Por parte del vendedor], él debe estar en sintonía con los requerimientos de la empresa, ocupándose de resolver los problemas y las situaciones que se le presenten al cliente, puesto que esto representa valor económico y crecimiento personal para el vendedor.
 
     p.mb-4 En la actualidad existen herramientas que contribuyen al mejoramiento de las relaciones con el cliente, lo que se denomina gestión de las relaciones, esta estrategia combina la tecnología de la información con la cultura organizacional, en la cual el principal objetivo es mantener excelentes relaciones y satisfacer las necesidades de los clientes. 
@@ -34,29 +34,29 @@
     .row.justify-content-center.justify-content-center
       .col-sm-12.col-lg-4.col-md-12.mb-4(data-aos="fade-right")
         .overflow-hidden.h-100.p-4
-          .row.align-items-center.justify-content-center.bgfondotema2.p-4
+          .row.align-items-center.justify-content-center.bg-color-10.p-4
             img.img95(src='@/assets/curso/temas/tema2/tar1-1.png', alt='').mb-4
             .row.justify-content-center.mb-3
               .col-12
-                .bgmoradomedio.p-1 
+                .bg-color-8.p-1 
                   h3.text-center.mb-0 Definición
             p.mb-5.text-center.p-4 Los protocolos de atención al cliente son la manera de fomentar en la organización la forma de actuar frente al cliente, el protocolo debe ser un documento que unifique los conceptos y criterios que lleven a cada colaborador a respetar y prestar una buena atención al cliente. Los protocolos deben destacar la imagen de la compañía y su identidad corporativa.
       .col-sm-12.col-lg-4.col-md-12.mb-4(data-aos="fade-right")
         .overflow-hidden.h-100.p-4
-          .row.align-items-center.justify-content-center.bgfondotema2.p-4
+          .row.align-items-center.justify-content-center.bg-color-10.p-4
             img.img95(src='@/assets/curso/temas/tema2/tar1-2.png', alt='').mb-4
             .row.justify-content-center
               .col-12
-                .bgmoradomedio.p-1 
+                .bg-color-8.p-1 
                   h3.text-center.mb-0 Importancia
             p.text-center.p-4 La importancia que tiene un protocolo de atención y servicio al cliente radica en la manera cómo los asesores comerciales deben comportarse al atender a los clientes, explica la forma de actuar de los trabajadores en la atención al cliente o en el proceso de ventas, este documento detalla los procedimientos de interacción con el cliente, también debe contener los planes de mejora cuando se presenten hallazgos que desvíen la prestación del servicio.
       .col-sm-12.col-lg-4.col-md-12.mb-4(data-aos="fade-right")
         .overflow-hidden.h-100.p-4
-          .row.align-items-center.justify-content-center.bgfondotema2.p-4
-            img.img95(src='@/assets/curso/temas/tema2/tar1-2.png', alt='').mb-4
+          .row.align-items-center.justify-content-center.bg-color-10.p-4
+            img.img95(src='@/assets/curso/temas/tema2/tar1-3.png', alt='').mb-4
             .row.justify-content-center.mb-3
               .col-12
-                .bgmoradomedio.p-1 
+                .bg-color-8.p-1 
                   h3.text-center.mb-0 Manual
             p.mb-4.text-center.p-4 El medio para lograrlo es elaborar un manual de protocolo de atención al cliente en forma clara y didáctica, comprensible para todos. Este manual debe explicar los procedimientos que se desea de los colaboradores, la conducta que deben tener con los usuarios o clientes y los procedimientos adecuados con relación a la atención que se quiere brindar en cualquier canal de contacto.
 
@@ -68,21 +68,21 @@
           .col-lg-3.sobavatar2.d-none.d-lg-block(data-aos="fade-right")
             img(src="@/assets/curso/temas/tema2/avar1-1.png", alt="alt")
           .col-lg-9(data-aos="fade-left")
-            .bgrosado2.borde-redondo-arriba.p-3
+            .bg-color-11.borde-redondo-arriba.p-3
               h3.moverderecha2.mt-4.mb-2 Atención robusta al cliente
               p.moverderecha2.mb-4 Cada vez que se repite la misma acción entre los trabajadores, el servicio se perfecciona.
         .row.justify-content-center
           .col-lg-3.sobavatar2.d-none.d-lg-block(data-aos="fade-right")
             img(src="@/assets/curso/temas/tema2/avar1-2.png", alt="alt")
           .col-lg-9(data-aos="fade-left")
-            .bgmoradomedio.p-3
+            .bg-color-2.p-3
               h3.moverderecha2.mt-4.mb-2 Procesos iguales en la atención al cliente
               p.moverderecha2.mb-4 Para cada momento de encuentro con el cliente desde su llegada hasta en la postventa debe ser el mismo método de trabajo.
         .row.justify-content-center
           .col-lg-3.sobavatar2.d-none.d-lg-block(data-aos="fade-right")
-            img(src="@/assets/curso/temas/tema2/avar1-2.png", alt="alt")
+            img(src="@/assets/curso/temas/tema2/avar1-3.png", alt="alt")
           .col-lg-9(data-aos="fade-left")
-            .bgrosado2.borde-redondo-abajo.p-3
+            .bg-color-9.borde-redondo-abajo.p-3
               h3.moverderecha2.mt-4.mb-2 Capacitación permanente
               p.moverderecha2.mb-4 En cada uno de los procesos lo que permite un alto nivel de calidad.
       .col-lg-4.p-0
@@ -94,7 +94,7 @@
         .col-12.col-md-8.col-lg-4
           img(src='@/assets/curso/temas/tema2/img2.png', alt='')(data-aos="fade-left")
         .col-sm-12.col-md-12.col-lg-8.mb-0.mt-4
-          .row.bgavatarazulclaro.ps-4.mb-0.p-3
+          .row.bg-color-10.ps-4.mb-0.p-3
             ol.lista-ol--cuadro.lista-ol--separador.fa-ol.ps-3
               li
                 .lista-ol--cuadro__vineta
@@ -123,28 +123,28 @@
 
     .row.justify-content-center.align-items-center.mb-5
       .col-sm-12.col-md-12.col-lg-8.mt-3
-        .row.bgrosado.mt-3
+        .row.bg-color-2.mt-3
           ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4
             li.mb-0 
               i.fas.fa-check-circle
               span.mb-0 #[b Saludo de bienvenida para el cliente.]
             p.ps-4 Buenos días (tardes, noche), mi nombre es Alba, en qué puedo ayudarlo. (Acompañado de un rostro amable).
-        .row.bgrosado.mt-3
+        .row.bg-color-9.mt-3
           ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
             li.mb-0 
               i.fas.fa-check-circle
               span.mb-0 Identificar su necesidad e invitarlo a realizar el recorrido al lugar de su necesidad.
-        .row.bgrosado.mt-3
+        .row.bg-color-2.mt-3
           ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
             li.mb-0 
               i.fas.fa-check-circle
               span.mb-0 Escuchar de manera atenta las inquietudes del cliente, haciendo preguntas hasta identificar su necesidad.
-        .row.bgrosado.mt-3
+        .row.bg-color-9.mt-3
           ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
             li.mb-0 
               i.fas.fa-check-circle
               span.mb-0 Dar la información clara sobre beneficios y ventajas del producto según su necesidad y, brindarle productos complementarios.
-        .row.bgrosado.mt-3
+        .row.bg-color-2.mt-3
           ul.lista-ul--color.fa-al.ps-5.mb-0.mt-4.mb-4
             li.mb-0 
               i.fas.fa-check-circle
